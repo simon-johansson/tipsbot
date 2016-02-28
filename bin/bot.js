6dca73d9eb45488152c46d1b0005b8750c40650f
Simon Johansson <mail@simon-johansson.com>
2016-02-28 20:52:46 +0100

    refactor(bin): Fix to bin/ dir, now points to build/ dir instead of lib/

--- bin/bot.js
+++ bin/bot.js
@@ -1,22 +1,25 @@
 
-import { existsSync } from 'fs';
-import { resolve } from 'path';
-import TipsBot from '../lib/tipsbot';
+var existsSync = require('fs').existsSync;
+var resolve = require('path').resolve;
+var TipsBot = require('../build/tipsbot').default;
 
-const tokenPath = resolve(__dirname, '..', 'token.js');
-const defaultToken = existsSync(tokenPath) ? require('../token') : null;
-const defaultName = 'Tipsbot';
-const defaultTips = resolve(__dirname, '..', 'data', 'pragmatic-programmer.json');
-const defaultChannel = 'general';
-const defaultSchedule = '0 9 * * 1,2,3,4,5'; // 09:00 on monday-friday
-const defaultStartIndex = 0;
+var tokenPath = resolve(__dirname, '..', 'token.js');
+var defaultToken = existsSync(tokenPath) ? require('../token') : null;
+var defaultName = 'Tipsbot';
+var defaultTips = resolve(__dirname, '..', 'data', 'pragmatic-programmer.json');
+var defaultChannel = 'general';
+var defaultSchedule = '0 9 * * 1,2,3,4,5'; // 09:00 on monday-friday
+var defaultStartIndex = 0;
 
-export const create = () =>
-  new TipsBot({
-    token: process.env.BOT_API_KEY || defaultToken,
-    name: process.env.BOT_NAME || defaultName,
-    filePath: process.env.BOT_FILE_PATH || defaultTips,
-    channel: process.env.BOT_CHANNEL || defaultChannel,
-    schedule: process.env.BOT_SCHEDULE || defaultSchedule,
-    startIndex: process.env.BOT_START_INDEX || defaultStartIndex,
-  });
+module.exports = {
+  create: function () {
+    return new TipsBot({
+      token: process.env.BOT_API_KEY || defaultToken,
+      name: process.env.BOT_NAME || defaultName,
+      filePath: process.env.BOT_FILE_PATH || defaultTips,
+      channel: process.env.BOT_CHANNEL || defaultChannel,
+      schedule: process.env.BOT_SCHEDULE || defaultSchedule,
+      startIndex: process.env.BOT_START_INDEX || defaultStartIndex,
+    });
+  }
+}
--- bin/index.js
+++ bin/index.js
@@ -3,18 +3,10 @@
 'use strict';
 
 /**
- * TipsBot launcher script.
+ * Tipsbot launcher script.
  *
  * @author Simon Johansson <simon.johansson@screeninteraction.com>
  */
 
- require('babel-polyfill');
- require('babel-register')({
-   presets: ['es2015', 'stage-0'],
-   'plugins': [
-     ['transform-decorators-legacy'],
-   ]
- });
-
-var bot = require('./bot').create();
-bot.run();
+require('babel-polyfill');
+require('./bot').create().run();
